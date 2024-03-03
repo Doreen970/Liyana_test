19e7201624b4cf95710e1745d11441e95d0f1b8d
Doreen970 <doreenwanyama20@gmail.com>
2024-03-03 23:36:29 +0300

    more changes

--- search_feature/react_search/src/components/SearchBar.jsx
+++ search_feature/react_search/src/components/SearchBar.jsx
@@ -2,14 +2,18 @@ import React, {useState} from "react"
 import {FaSearch} from "react-icons/fa"
 import "./search.css"
 
+// a component that handles the data added in the input field
 export const SearchBar = ({ setResults }) => {
     const [input, setInput] = useState("")
     
     const fetchData = (value) => {
+        // the API that contains our data
 fetch("https://jsonplaceholder.typicode.com/users")
+// conversion of the response to json format
 .then((response) => response.json())
 .then((json) => {
     const results = json.filter((user) => {
+        // a feature that ensures only results that match the search criteria are returned. 
         return value && user && user.name && user.name.toLowerCase().includes(value)
     })
     setResults(results)
--- search_feature/react_search/src/components/resultsdata.jsx
+++ search_feature/react_search/src/components/resultsdata.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import "./resultsdata.css"
 
 export const SearchData = ({ result }) => {
+    // This is a component that handles all the data returned after a user search
     return (
         <div className="search-data">{result.name}</div>
     )
