68899c7e0dea9f71043a8ddbf25b155383c81017
Doreen970 <doreenwanyama20@gmail.com>
2024-03-04 00:31:14 +0300

    crucial changes

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 import requests
 
@@ -6,8 +7,8 @@ app = Flask(__name__)
 @app.route('/my_test')
 def test_api():
     url = "https://xml.avra.vacations/services/webservice.asmx?op=HotelsSearch"
-    username = 'liyanahotels'
-    password = '@60033*'
+    username = os.environ.get('USERNAME')
+    password = os.environ.get('PASSWORD')
 
     try:
         response = requests.get(url, auth=(username, password))
@@ -17,7 +18,7 @@ def test_api():
             return jsonify({"message": "Successfully tested the endpoint"}), 200
         elif response.status_code == 404:
             # Handle page not found error
-            return jsonify({"error": "Page not found"}), 404
+            return jsonify({"message": "Page not found"}), 404
         else:
             return jsonify({"error": "Unexpected status code"}), 500    
     except requests.RequestException as e:
